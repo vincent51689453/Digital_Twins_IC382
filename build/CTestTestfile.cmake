24832e6cf16cb249c85abaaa2a6f47645fc99d41
vincent51689453 <vincent51689453@gmail.com>
2020-08-21 10:29:53 +0800

    init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/vincent/vincent_dev/gazebo_ws/src
+# Build directory: /home/vincent/vincent_dev/gazebo_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot_gazebo")
---
+++ build/robot_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vincent/vincent_dev/gazebo_ws/src/robot_gazebo
+# Build directory: /home/vincent/vincent_dev/gazebo_ws/build/robot_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
